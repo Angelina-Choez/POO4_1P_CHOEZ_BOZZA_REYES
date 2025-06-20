0db6ff51ff3ba752a45c0a3ccb4d118a40e1fee3
PaoReyes <reyespaola3022@gmail.com>
2025-06-19 22:24:53 -0500

    Actualizando

--- proyecto/src/main/java/com/proyecto/com/proyecto/Producto.java
+++ proyecto/src/main/java/com/proyecto/com/proyecto/Producto.java
@@ -15,19 +15,39 @@ public class Producto {
         this.categoria = categoria;
     }
 
-    public String getCodigo() { return codigo; }
-    public void setCodigo(String codigo) { this.codigo = codigo; }
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
 
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-    public double getPrecio() { return precio; }
-    public void setPrecio(double precio) { this.precio = precio; }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio){
+        this.precio = precio;
+    }
 
-    public int getStock() { return stock; }
-    public void setStock(int stock) { this.stock = stock; }
+    public int getStock() {
+        return stock;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 
-    public String getCategoria() { return categoria; }
-    public void setCategoria(String categoria) { this.categoria = categoria; }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
 
